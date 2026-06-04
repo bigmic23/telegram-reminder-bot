4d3d22c08bcab31d57967b72c576688a7b8a454b
Michael Sonwu <michael.sonwu.careers@gmail.com>
2026-06-04 20:04:17 +0100

    fix clean bot structure

--- bot.js
+++ bot.js
@@ -1,16 +1,29 @@
 console.log("BOT START TEST");
 
 require("dotenv").config();
+const express = require("express");
 const { Telegraf } = require("telegraf");
 
+const app = express();
+app.use(express.json());
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+/* ---------------- TELEGRAM BOT ---------------- */
 
 bot.start((ctx) => {
   ctx.reply("Bot is alive and connected.");
 });
 
-const express = require("express");
-const app = express();
+bot.on("text", (ctx) => {
+  ctx.reply("Received: " + ctx.message.text);
+});
+
+bot.telegram.getMe().then((info) => {
+  console.log("Telegram connected as:", info.username);
+});
+
+/* ---------------- EXPRESS SERVER ---------------- */
 
 app.get("/", (req, res) => {
   res.send("Bot is alive");
@@ -21,5 +34,7 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+/* ---------------- START BOT ---------------- */
 
 bot.launch();
