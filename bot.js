ac2f385c28fa1400628c0c284164c4a32682b6e7
Michael Sonwu <michael.sonwu.careers@gmail.com>
2026-06-04 19:18:17 +0100

    keep render process alive

--- bot.js
+++ bot.js
@@ -1,5 +1,14 @@
 console.log("BOT START TEST");
 
-setTimeout(() => {
-  console.log("RUNNING OK");
-}, 5000);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
