32088ebd0822a42e1658b812fb834807b7a30731
Michael Sonwu <michael.sonwu.careers@gmail.com>
2026-06-05 07:17:28 +0100

    restore db init

--- bot.js
+++ bot.js
@@ -2,17 +2,27 @@ require("dotenv").config();
 
 const express = require("express");
 const { Telegraf } = require("telegraf");
+const { initDB } = require("./db");
 
 const app = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+(async () => {
+  try {
+    await initDB();
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.error("DB INIT ERROR:", err);
+  }
+})();
 
-bot.start((ctx) => {
-  ctx.reply("Bot is alive and connected.");
+bot.start((ctx) => { ctx.reply("Bot is 
+  alive and connected.");
 });
 
-bot.on("text", (ctx) => {
-  ctx.reply("Received: " + ctx.message.text);
+bot.on("text", (ctx) => { 
+  ctx.reply("Received: " + 
+  ctx.message.text);
 });
 
 bot.telegram.getMe()
